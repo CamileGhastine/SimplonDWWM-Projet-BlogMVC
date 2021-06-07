22f4e31923e684d41066b98587a1b779e57208f2
CamileGhastine <camileghastine@hotmail.com>
2021-06-07 10:40:18 +0100

    add public reportory

--- public/index.php
+++ public/index.php
@@ -1,21 +1,23 @@
 <?php
 
+$path = dirname(__DIR__);
+
 ob_start();
 try {
     $page = isset($_GET['page']) ? $_GET['page'] : 'post.home';
 
     if ($page === 'post.home') {
-        require 'view/post/home.php';
+        require $path . '/view/post/home.php';
     } elseif ($page === 'post.show') {
-        require 'view/post/show.php';
+        require $path . '/view/post/show.php';
     } elseif ($page === 'user.connect') {
-        require 'view/user/connectionForm.php';
+        require $path . '/view/user/connectionForm.php';
     } else {
         throw new Exception('404');
     }
 } catch (Exception $e) {
-    require 'view/error/error' . $e->getMessage() . '.php';
+    require $path . '/view/error/error' . $e->getMessage() . '.php';
 }
 $content = ob_get_clean();
 
-require 'view/base.php';
+require $path . '/view/base.php';
