54f6f3c37f4f5cc80fca3c84f7296fdc7751f203
CamileGhastine <camileghastine@hotmail.com>
2021-06-07 11:07:33 +0100

    refactore

--- model/postRepository.php
+++ model/postRepository.php
@@ -4,16 +4,7 @@
 
 function findAll()
 {
-    try {
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-        ];
-        $db = new PDO('mysql:host=localhost;dbname=blog', 'root', '', $options);
-    } catch (PDOException $e) {
-        print "Erreur !: " . $e->getMessage() . "<br/>";
-        die();
-    }
+    $db = getDBConnection();
 
     $request = $db->query('SELECT id, title, LEFT(content, 150) as content, date, user FROM post');
     $request->setFetchMode(PDO::FETCH_ASSOC);
@@ -27,16 +18,7 @@ function findAll()
 
 function findOneById($id)
 {
-    try {
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-        ];
-        $db = new PDO('mysql:host=localhost;dbname=blog', 'root', '', $options);
-    } catch (PDOException $e) {
-        print "Erreur !: " . $e->getMessage() . "<br/>";
-        die();
-    }
+    $db = getDBConnection();
 
     $request = $db->prepare('SELECT * FROM post WHERE id=?');
     $request->execute([$id]);
@@ -47,4 +29,18 @@ function findOneById($id)
     $request->closeCursor();
 
     return $post;
+}
+
+function getDBConnection()
+{
+    try {
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        ];
+        return new PDO('mysql:host=localhost;dbname=blog', 'root', '', $options);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    }
 }
